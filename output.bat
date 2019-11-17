8aa64b5cd0c2b07d1c7bf738dff5e35448eff52b
osyfeddine <45755994+osyfeddine@users.noreply.github.com>
2019-11-18 00:29:46 +0100

    Update output.bat

--- output.bat
+++ output.bat
@@ -1,1 +1,55 @@
-copy C:\sites\backblue.gif %1
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                DateTime localDate = DateTime.Now;
+
+                string reportingTme = localDate.TimeOfDay.ToString("hh");
+                string path = Directory.GetCurrentDirectory();
+                string dtRep = localDate.Date.ToString("yyyyMMdd");
+                string paramBat = dtRep + reportingTme + "00";
+
+                Process proc = new Process();
+                proc.StartInfo.WorkingDirectory = path;
+                Console.WriteLine(" 1:input\n 2:output\n--------------------------------------------------------");
+                int v = Convert.ToInt32(Console.ReadLine());
+
+                if (v == 1)
+                {
+                    proc.StartInfo.FileName = "input.bat";
+                    proc.StartInfo.Arguments = " " + paramBat;
+                }
+
+                else
+                {
+                    proc.StartInfo.FileName = "output.bat";
+                    proc.StartInfo.Arguments = " " + paramBat;
+                }
+
+                proc.StartInfo.CreateNoWindow = false;
+                proc.Start();
+             
+                proc.WaitForExit();
+                Console.WriteLine("ok");
+                Console.Read();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error: "+e.Message);
+            }
+           
+        }
+    }
+}
